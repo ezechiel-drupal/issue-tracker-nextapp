071cf339186f96770b6eed54345a5b98e45fcb08
Ézéchiel JOHNSTONE <ezechiel.johnstone@actency.fr>
2024-07-19 19:07:08 +0200

    Built a new page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
+import { Button, TextField } from "@radix-ui/themes";
+import React from "react";
+import { TextArea } from "@radix-ui/themes";
 
-const page = () => {
+const NewIssuePage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Search the docs…">
+        <TextField.Slot></TextField.Slot>
+      </TextField.Root>
+      <TextArea placeholder="Reply to comment…" />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+};
 
-export default page
+export default NewIssuePage;
