1cd0e64426abc960a6b45bb3fddcd332da6cf6d6
Ézéchiel JOHNSTONE <ezechiel.johnstone@actency.fr>
2024-07-23 12:20:15 +0200

    Build an API for updating

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,16 +1,10 @@
-import { Prisma } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 import prisma from "@/prisma/client";
-
-const createIssueSchema = z.object({
-  title: z.string().min(1, "Title is required.").max(255),
-  description: z.string().min(1, "Description is required."),
-});
+import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from "./createIssueSchema";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = issueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
