338706d876cb86117c66879031acc45afc3ba09a
Ézéchiel JOHNSTONE <ezechiel.johnstone@actency.fr>
2024-07-23 16:30:03 +0200

    Improve the user experience

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,7 +1,6 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "../createIssueSchema";
-import { error } from "console";
 
 export async function PATCH(
   request: NextRequest,
