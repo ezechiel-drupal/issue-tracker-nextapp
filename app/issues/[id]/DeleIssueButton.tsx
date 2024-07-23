52c7257104da7f7e0a353abf2c2489be5320dfda
Ézéchiel JOHNSTONE <ezechiel.johnstone@actency.fr>
2024-07-23 14:53:09 +0200

    Add a delete button

---
+++ app/issues/[id]/DeleIssueButton.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@radix-ui/themes";
+
+const DeleIssueButton = ({ issueId }: { issueId: number }) => {
+  return <Button color="red">Delete Issue</Button>;
+};
+
+export default DeleIssueButton;
